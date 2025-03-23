4786a804ec922dcd8a06cb269ceefc661d67b871
Felipe Oliveira <felipe.oliveira@quikdev.com.br>
2025-03-23 13:12:47 -0300

    feat: resolve nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from 'next';
 
-const isExport = process.env.NEXT_PUBLIC_IS_EXPORT === 'true';
-
-const nextConfig: NextConfig = {
-  output: isExport ? 'export' : undefined, // Apenas define 'export' se for exportação
-  trailingSlash: true,
-  images: {
-    unoptimized: isExport, // Desabilita otimização apenas no export
-  },
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
